95803d2904628a03409f9133506a301b5c77b02a
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-26 12:51:48 +0800

    [HACK] mi89xx: libinit: Set props to allow debugging

--- libraries/libinit/init_xiaomi_mi89xx.cpp
+++ libraries/libinit/init_xiaomi_mi89xx.cpp
@@ -5,9 +5,21 @@
  */
 
 #include <libinit_dalvik_heap.h>
+#include <libinit_utils.h>
 
 #include "vendor_init.h"
 
+void set_debugging_props() {
+    property_override("persist.sys.usb.config", "adb");
+    property_override("ro.adb.secure", "0");
+    property_override("ro.debuggable", "1");
+    property_override("ro.boot.verifiedbootstate", "orange");
+    property_override("ro.secure", "0");
+    set_ro_build_prop("build.tags", "test-keys");
+    set_ro_build_prop("build.type", "eng");
+}
+
 void vendor_load_properties() {
     set_dalvik_heap();
+    set_debugging_props();
 }
